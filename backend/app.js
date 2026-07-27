b9e0ab80d3337b7e66d44aa68ef5b68cb739f616
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-07-27 18:06:00 +0200

    Deleted manual headers, helmet is enough

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,14 @@
 import express from "express";
+import chalk from "chalk";
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import { config } from "./config/serverConfig.js";
+
+dotenv.config();
+
 const app = express();
+
+app.use(helmet());
 
 export default app;
