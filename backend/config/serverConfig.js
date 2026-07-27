0d7a5ffee38a0860d3964e29c54b6ae9dbbee830
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-07-27 17:57:30 +0200

    Backend startup

---
+++ backend/config/serverConfig.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const PORT = process.env.SERVER_PORT || 3000;
+export const FRONT_PORT = process.env.FRONT_PORT || 5173;
+export const ENV = process.env.NODE_ENV || "development";
+export const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const productionOrigin = FRONTEND_URL || "...";
+
+export const config = {
+  server: {
+    rateLimit: {
+      windowMs: 10 * 60 * 1000,
+      max: 50,
+    },
+    corsOptions: {
+      origin:
+        ENV === "production"
+          ? [productionOrigin]
+          : [`http://localhost:${FRONT_PORT}`],
+      methods: "POST,GET,PUT,DELETE",
+      allowedHeaders: "Content-Type,Authorization",
+      credentials: true,
+    },
+  },
+};
---
+++ backend/server.js
@@ -0,0 +1,7 @@
+import { PORT } from "./config/serverConfig.js";
+import chalk from "chalk";
+import app from "./app.js";
+
+app.listen(PORT, () =>
+  console.log(chalk.blue(`\n Ticket app listening on port ${PORT}!\n`)),
+);
