926799c19dc29365b24f2591db4e6e4223e361ad
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-07-29 00:17:37 +0200

    Added register test

---
+++ backend/tests/authService.test.js
@@ -0,0 +1,12 @@
+import AuthServices from "../services/authServices";
+const auth = new AuthServices();
+
+test("Should register user", async () => {
+  const user = await auth.registerService({
+    username: "TestUser",
+    email: "test@test.com",
+    password: "User123!",
+  });
+
+  expect(user.email).toBe("test@test.com");
+});
