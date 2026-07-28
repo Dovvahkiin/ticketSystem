55788ba90182e84ebfd0e43221a465b3d9636d10
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-07-29 00:16:15 +0200

    Fixed minor bugs and syntax

--- backend/services/authServices.js
+++ backend/services/authServices.js
@@ -1,20 +1,19 @@
 import User from "../models/userModel";
-import AuthValidator from "../validators/authValidator";
 import chalk from "chalk";
 import bcrypt from "bcrypt";
 const saltRounds = 5;
-const validate = new AuthValidator();
 
-class authServices {
-  register = async (data) => {
+class AuthServices {
+  registerService = async (data) => {
     const { username, email, password } = data;
+
     const existingUser = await User.findOne({ email });
-    if (existingUser) throw new error(chalk.red("User already exists."));
+    if (existingUser) throw new Error(chalk.red("User already exists."));
 
     const passwordHash = await bcrypt.hash(password, saltRounds);
     console.log("Password is hashed!");
 
-    const user = User.create({
+    const user = await User.create({
       username,
       email,
       password: passwordHash,
@@ -29,4 +28,4 @@ class authServices {
   };
 }
 
-export default authServices;
+export default AuthServices;
