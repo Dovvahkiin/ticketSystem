f6fe1e2e7e15cf9edbbcab0c0d0f50ab01e3b183
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-07-30 01:38:13 +0200

    Added login and logout routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,17 @@
 import express from "express";
 import UserControllers from "../controllers/userController.js";
+import verifyRToken from "../middleware/refreshToken.js";
+import AuthenticationFunctions from "../middleware/auth.js";
 
 const userCtrl = new UserControllers();
 const userRouter = express.Router();
 
-console.log("LOading");
+console.log("Loading");
 
 userRouter.route("/register").post(userCtrl.registerController);
+userRouter.route("/logout").post(userCtrl.logoutController);
+userRouter
+  .route("/login")
+  .post(AuthenticationFunctions.checkLoginAuth, userCtrl.loginController);
 
 export default userRouter;
