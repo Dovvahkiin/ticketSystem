919385b749799aeee4a9457ef02fd951c18bd756
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-07-30 01:37:33 +0200

    Added cookie parser

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import chalk from "chalk";
 import cors from "cors";
 import helmet from "helmet";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { config } from "./config/serverConfig.js";
 import { limiter } from "./middleware/rateLimiter.js";
 import userRouter from "./routes/userRoutes.js";
@@ -15,6 +16,7 @@ app.use(helmet());
 app.use(cors(config.server.corsOptions));
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(limiter);
