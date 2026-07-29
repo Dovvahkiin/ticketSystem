9d162006abacf3cc82dc38c20a46647e91fedf9e
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-07-30 01:40:21 +0200

    Added login and logout controllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import WhichValidator from "../validators/authValidator.js";
 import AuthServices from "../services/authServices.js";
+import { setCookies, clearCookies } from "../utils/setCookies.js";
 const auth = new AuthServices();
 
 class UserControllers {
@@ -28,6 +29,41 @@ class UserControllers {
         error: error.message,
       });
     }
+  };
+
+  loginController = async (req, res) => {
+    try {
+      const data = req.body;
+      const validationResult = WhichValidator.usingValidator("login", data);
+
+      if (!validationResult.success) {
+        return res.status(400).json({
+          message: "Validation failed!",
+          error: validationResult.error.issues,
+        });
+      }
+
+      const tokens = await auth.loginService(validationResult.data);
+      console.log(tokens);
+
+      setCookies(res, tokens);
+
+      return res.status(200).json({
+        message: "User is logged in!",
+        tokens,
+        success: true,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: "Internal Server Error!",
+        error: error.message,
+      });
+    }
+  };
+
+  logoutController = async (req, res) => {
+    clearCookies(res);
+    return res.status(200).json({ message: "You are logged out!" });
   };
 }
 
